15cdde2c3cf45138bc0a3b7b4da8a6a247cf1499
Sophie Helf <sophie.helf@gmail.com>
2017-02-09 14:33:47 -0800

    less prototype more action

--- calculator.js
+++ calculator.js
@@ -1,22 +1,38 @@
 var firstNumber = 0;
 var nextNumber = 0;
 var toSquish = [];
-var operand;
-var operandPressed = false;
+// var operandPressed = false;
 
 var digit = document.getElementsByClassName("num");
 var operand = document.getElementsByClassName("operand");
 
-// log digit
+// log digit + push to array
+
 function logIt(){
-  console.log("you pushed me dude");
+  var digitToPush = this.innerText;
+  console.log(digitToPush);
+  toSquish.push(digitToPush);
+  console.log(toSquish);
 }
 
 for (var i = 0; i < digit.length; i++){
   digit[i].onclick = logIt;
 }
 
-// log operand 
+// on operand click
+// assign specific operand to variable
+// join + parsefloat digits in toSquish
+// this = firstnumber
+
+function digitReduce(){
+  toSquish.join('');
+  console.log(toSquish.join(''));
+}
+
+for (var i = 0; i < operand.length; i++) {
+  operand[i].onclick = digitReduce;
+}
+
 
 /////////// functions
 
