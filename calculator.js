04b516401de9d7725a42d3ddeecdc0f833bb9045
Sophie Helf <sophie.helf@gmail.com>
2017-02-11 22:58:24 -0800

    insert anakin skywalker meme here

--- calculator.js
+++ calculator.js
@@ -2,6 +2,7 @@
 //var nextNumber = 0;
 var toSquish = [];
 var toCompute = [];
+var answer = 0;
 var operandClicked;
 var operandPushes = 0;
 
@@ -9,7 +10,18 @@ var digit = document.getElementsByClassName("num");
 var operand = document.getElementsByClassName("operand");
 
 
-// log digit + push to array
+// loops
+
+for (var i = 0; i < operand.length; i++) {
+  operand[i].onclick = condense;
+}
+
+for (var i = 0; i < digit.length; i++){
+  digit[i].onclick = logIt;
+}
+
+
+// onclick functions
 
 function logIt(){
   var digitToPush = this.innerText;
@@ -17,73 +29,48 @@ function logIt(){
   console.log(toSquish);
 }
 
-for (var i = 0; i < digit.length; i++){
-  digit[i].onclick = logIt;
-}
-
-// on operand click
-// assign specific operand to variable
-// join + parsefloat digits in toSquish
-
 function condense(){
   var number = parseFloat(toSquish.join(''));
   console.log(number);
   toCompute.push(number);
   console.log(toCompute);
   toSquish = [];
-  operandLog();
-  operandCountPlus();
-}
-
-function operandLog() {
+  
+  // log operand clicked
   operandClicked = this.innerHTML
   console.log(operandClicked);
-}
-
-function operandCountPlus() {
+  
+  // count how many times operand has been pushed
   operandPushes++;
   console.log(operandPushes);
-}
-
-for (var i = 0; i < operand.length; i++) {
-  operand[i].onclick = condense;
-}
-
-// computational stuff innit
-function arithmetic(a,b){
-switch(operandClicked) {
-  case "+":
-    toCompute.reduce(function(a,b){
-      return a+b;
-    })
-    break;
-  case "-":
-    toCompute.reduce(function(a,b){
-      return a-b;
-    })
-    break;
-  case "x":
-    toCompute.reduce(function(a,b){
-      return a*b;
-    })
-    break;
-  case "÷":
-    toCompute.reduce(function(a,b){
-      return a/b;
-    })
-    break;
-                     }
-}
-//// begin the while loop binches 
-
-while (toCompute.length > 0) {
+  
   if (operandPushes > 1) {
-    var result = arithmetic();
-    console.log(result);
-    toCompute = [result];
-    operandPushes++
+    arithmetic();
   }
-  else {
-    
+}
+
+function clearComputeArray(){
+  toCompute = [];
+}
+
+
+/// arithmetic functions
+
+function arithmetic() {
+  if(operandClicked == "+"){
+    answer = toCompute[0] + tocompute[1];
+    console.log(answer);
+  }
+  else if(operandClicked == "-"){
+    answer = toCompute[0] - toCompute[1];
+    console.log(answer);
+  }
+  else if (operandClicked == "x") {
+    answer = toCompute[0] * toCompute[1];
+    console.log(answer);
+  }
+  else if (operandClicked == "÷") {
+    answer = toCompute[0] / toCompute[1];
+    console.log(answer);
   }
 }
